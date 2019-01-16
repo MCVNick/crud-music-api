a56da124ca3c97413e1c37daf4e037182441ef11
MCVNick <mcvnick@gmail.com>
2019-01-16 08:09:43 -0700

    attempted to make header more customizable

--- src/App.js
+++ src/App.js
@@ -5,10 +5,32 @@ import './App.css';
 import Header from './Components/Header/Header'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      companyName: 'Jiggy',
+      companyPrimaryColor: '#666666',
+      companyPrimaryTextColor: '',
+      companySecondaryColor: '#666666',
+      companySecondaryTextColor: '',
+      companyTertiaryColor: '#666666',
+      companyTertiaryTextColor: '',
+    }
+  }
   render() {
+    const {
+      companyName,
+      companyPrimaryColor,
+      companyPrimaryTextColor,
+      companySecondaryColor,
+      companySecondaryTextColor,
+      companyTertiaryColor,
+      companyTertiaryTextColor,
+    } = this.state
     return (
       <div className="App">
-        <Header companyName={'Jiggy'}/>
+        <Header companyName={companyName} companyPrimaryColor={companyPrimaryColor} companyPrimaryTextColor={companyPrimaryTextColor}/>
       </div>
     );
   }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,9 +4,11 @@ import './Header.css'
 class Header extends Component{
     render() {
         return (
-            <h1 class='companyName'>
-                {this.props.companyName ? this.props.companyName : 'Enter a company name'}
-            </h1>
+            <div>
+                <h1 class='companyName'>
+                    {this.props.companyName ? this.props.companyName : 'Enter a company name'}
+                </h1>
+            </div>
         )
     }
 }
